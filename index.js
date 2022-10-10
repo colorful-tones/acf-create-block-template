97c395b0342c247f81187953e1f970ae9e18e68f
Damon Cook <damonacook@gmail.com>
2022-10-10 16:06:47 -0400

    📦 NEW: v0.1.5 w/ category and example field

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ module.exports = {
 	pluginTemplatesPath: join(__dirname, 'plugin'),
 	blockTemplatesPath: join( __dirname, 'block' ),
 	defaultValues: {
+		namespace: 'acf',
 		slug: 'acf-blocks-starter',
+		category: 'acf',
 		version: '0.1.0',
-		category: 'acf-blocks-starter',
-		editorStyle: 'file:./editor.css',
-		style: 'file:./frontend.css'
-	},
-	customBlockJSON: {
-		acf: {
-			"mode": "preview"
+		script: 'file:./index.js',
+		style: 'file:./index.css',
+		customScripts: {
+			'start': 'wp-scripts start --webpack-src-dir=blocks --webpack-copy-php',
+			'build': 'wp-scripts build --webpack-src-dir=blocks --webpack-copy-php'
 		},
+		folderName: './blocks'
 	},
 	variants: {
 		static: {},
 		dynamic: {
-			"render": "file:./render.php"
+			customBlockJSON: {
+				acf: {
+					"mode": "preview",
+					"renderTemplate": "render.php",
+				}
+			},
 		}
 	}
 };
